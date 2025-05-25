36c00d7f5e613266948aed3896c65015a1acea7d
Sheikh Sarim <149183842+SheikhSarim@users.noreply.github.com>
2025-05-25 23:40:20 +0500

    minor changes

--- lib/utils/constants/app_constants.dart
+++ lib/utils/constants/app_constants.dart
@@ -1,28 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:package_info_plus/package_info_plus.dart';
-
-// class AppConstants {
-//   static String appMainName = '';
-//   static String appVersion = '';
-//   static String appTitle = 'City Guide';
-//   static String appAuthor = 'Powered by: Sheikh Sarim';
-
-//   static const appMainColor = Color(0xFFFF543D);
-//   static const appSecondaryColor = Color(0xFFFAF4F4);
-//   static const appMainTextColor = Color(0xFF222222);
-//   static const appSecondaryTextColor = Colors.white;
-//   static const appStatusBarColor = Color(0xFF9F9F9F);
-//   static const appAccentColor = Color(0xFF9B9B9B);
-//   static const appContainerColor = Color(0xFFFFFFFF);
-//   static final appBackgroundColor = Colors.grey[100]; Color(0xFFF5F5F5)
-
-//   static Future<void> init() async {
-//     final info = await PackageInfo.fromPlatform();
-//     appMainName = info.appName;
-//     appVersion = info.version;
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
@@ -30,38 +5,43 @@ class AppConstants {
   static String appMainName = '';
   static String appVersion = '';
   static String appTitle = 'Laptop Harbor';
-  static String appAuthor = 'Powered by: Your Name';
+  static String appAuthor = 'Powered by: Shiekh Sarim';
 
   // 🎨 Color Palette
-  // static const primaryColor = Color(0xFF2C2C2C); // Charcoal Black
-  static const primaryColor = Color(0xFFFF543D);
-  static const appSecondaryColor = Color(0xFFE5E7EB); // Slate Gray
+  /// Primary & Secondary Colors
+  static const Color primaryColor = Color(0xFFFF543D);
+  static const Color appSecondaryColor = Color(0xFFE5E7EB); 
 
+  /// Text Colors
+  static const Color primaryTextColor = Color(0xFF1A1A1A); 
+  static const Color secondaryTextColor = Color(0xFF6E6E6E); 
+  static const Color invertTextColor = Color(0xFFE0E0E0);
 
-  static const priceGreen = Color(0xFF4CAF50); // Green for Price
+  /// Background & Surface
+  static const Color backgroundColor = Color(0xFFF9F9F9); 
+  static const Color surfaceColor = Color(0xFFFFFFFF);
+  static final Color appBackgroundColor = backgroundColor;
 
-  static const accentDarkGrey = Color(0xFF555555);
-  static const accentGrey = Color(0xFF9B9B9B);
+  /// Accent Colors
+  static const Color accentDarkGrey = Color(0xFF555555);
+  static const Color accentGrey = Color(0xFF9B9B9B);
 
-  static const backgroundColor = Color(0xFFF9F9F9); // Snow Gray
-  static const surfaceColor = Color(0xFFFFFFFF); // Soft White
+  /// Pricing Indicators
+  static const Color priceGreen = Color(0xFF4CAF50);
+  static const Color priceBlue = Color(0xFF42A5F5);
 
-  static const primaryTextColor = Color(0xFF1A1A1A); // Almost Black
-  static const secondaryTextColor = Color(0xFF6E6E6E); // Medium Grey
-  static const invertTextColor = Color(0xFFE0E0E0); // Medium Grey
+  /// Icon Colors
+  static const Color primaryIconColor = Color(0xFF1A1A1A);
 
-  static const appButtonColor = Color(0xFF1A1A1A);
+  /// Button & Card Colors
+  static const Color appButtonColor = Color(0xFF1A1A1A);
+  static const Color cardColor = surfaceColor;
 
-  static const primaryIconColor = Color(0xFF1A1A1A);
+  /// Status Bar
+  static const Color appStatusBarColor = primaryColor;
 
-  static const appStatusBarColor = primaryColor;
-
-  // You can use this for cards, modals, inputs, etc.
-  static const cardColor = surfaceColor;
-
-  // For shared use in themes if needed
-  static final appBackgroundColor = backgroundColor;
-  // static const secondarybackgroundColor = Color(0xFF2C2C2C);
+  /// Profile Screen
+  static const Color profilescreenBackground = Color(0xFF2C2C2C); // Charcoal Black
 
   static Future<void> init() async {
     final info = await PackageInfo.fromPlatform();
